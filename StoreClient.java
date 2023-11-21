3c8b058e253a87c617e65eaf6aa724a0c9c538e2
sourrbtw <rekwenib@iu.edu>
2023-11-21 17:55:10 -0500

    Added item and remove to StoreClient

--- StoreClient.java
+++ StoreClient.java
@@ -9,7 +9,11 @@ public class StoreClient {
             Registry registry = LocateRegistry.getRegistry(null);
             StoreInterface stub = (StoreInterface) registry.lookup("Store");
             String response = stub.registerUser("User", "1234");
+            String item = stub.addItem(new Item("1", " Corn", 20.0, "Yellow corn", 2));
+            String remove = stub.removeItem(Item.getId());
+            System.out.println(remove);
             System.out.println("Response: " + response);
+            System.out.println(item);
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
